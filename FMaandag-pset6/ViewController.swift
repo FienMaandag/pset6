1e1d1030c7f6d5a5f16e7c672a915c3617f5f5ba
FienMaandag <fien.maandag@gmail.com>
2017-05-19 12:54:16 +0200

    Initial Commit

---
+++ FMaandag-pset6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FMaandag-pset6
+//
+//  Created by Fien Maandag on 19-05-17.
+//  Copyright © 2017 Fien Maandag. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
